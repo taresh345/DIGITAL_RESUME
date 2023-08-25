39ab4298ecdaeb51ad958cb724eca1f223b7ebba
taresh345 <48683755+taresh345@users.noreply.github.com>
2023-08-25 23:25:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ profile_pic = Image.open(profile_pic)
 col1, col2= st.columns(2, gap="small")
 with col1:
 
-    st.image(profile_pic, width=230)
+    # st.image(profile_pic, width=230)
 
 with col2:
     st.title(NAME)
